7b6145e19af817ea311de4482779097ee5efe5ca
yangjunquan <1686881567@qq.com>
2020-08-21 17:40:12 +0800

    初始化frame的窗口

--- mytank/src/com/yjq/tank/T.java
+++ mytank/src/com/yjq/tank/T.java
@@ -1,7 +1,10 @@
 package com.yjq.tank;
 
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-    /**
+/**
     * @projectName： mytank
     * @ClassName: T
     * @Description: TODO(这里用一句话描述这个类的作用)
@@ -12,6 +15,19 @@ package com.yjq.tank;
 public class T {
 
 	public static void main(String[] args) {
+		Frame f = new Frame();
+		f.setSize(1000, 800);
+		f.setResizable(false);
+		f.setTitle("tank war");
+		f.setVisible(true);
 		
+		f.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent arg0) {
+				System.exit(0);
+			}
+			
+		});
 	}
 }
